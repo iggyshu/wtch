77236c858a2fc256cc1f893681bc4ff4796b6ed8
Iggy S. <iggyshu@gmail.com>
2025-12-13 14:03:15 +0100

    feat: watch memory usage

--- wtch.go
+++ wtch.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/shirou/gopsutil/v4/mem"
+)
+
+func clearScreen() {
+	fmt.Print("\033[H\033[2J")
+}
+
+func displayMemory() {
+	const (
+		KB = 1024
+		MB = 1024 * KB
+	)
+
+	v, _ := mem.VirtualMemory()
+	totalMB := v.Total / MB
+	availableMB := v.Available / MB
+	usedPercent := v.UsedPercent
+
+	fmt.Printf("Total:      %d MB\n", totalMB)
+	fmt.Printf("Available:  %d MB\n", availableMB)
+	fmt.Printf("Used:       %.1f %%\n", usedPercent)
+}
+
+func watch() {
+	for {
+		clearScreen()
+		fmt.Println("wtch")
+		displayMemory()
+		time.Sleep(1 * time.Second)
+	}
+}
 
 func main() {
-	fmt.Println("wtch")
+	watch()
 }
